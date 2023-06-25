0c3b85a9d799e437da142d6463d53fbd95934637
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2023-06-25 20:09:43 +0100

    adjusments for build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
 module.exports = {
     images: {
       domains: ['fakestoreapi.com'],
